88119ccfe385d915dfa332c1b2e5dc30aec6fe50
FrancoParedes1235 <111542808+FrancoParedes1235@users.noreply.github.com>
2023-08-21 01:18:15 -0500

    parte 11 practica

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,42 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static String elegir(int vida) {
+        String gift = "";
+        switch (vida) {
+            case 1:
+                gift = "Un pasaje al caribe";
+                break;
+            case 2:
+                gift = "Una visita al museo más cercano a tu casa";
+                break;
+            case 3:
+                gift = "Una entrada al cine";
+                break;
+        }
+        return gift;
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        int vida,rp;
-        boolean fl=false;
-        vida=0;
-        Scanner sc=new Scanner(System.in);
+
+        int vida, rp;
+        boolean fl = false;
+        vida = 0;
+        Scanner sc = new Scanner(System.in);
         do {
+
             vida++;
-            System.out.println("Vida:"+vida+"\t¿Cual es el numero secreto?");
-            rp=sc.nextInt();
-            if(rp==1234)
-                fl=true;
-        }while (vida<3 && !fl);;
-        if (fl==true)
+            System.out.println("Vida:" + vida + "\t¿Cual es el numero secreto?");
+            rp = sc.nextInt();
+            if (rp == 1234)
+                fl = true;
+        } while (vida < 3 && !fl);
+        ;
+        if (fl == true) {
             System.out.println("Adivinaste");
-        else
+            System.out.println("¡Ganaste un cafe!");
+            System.out.println(elegir(vida));
+        } else
             System.out.println("ups,perdiste");
     }
 }
