2c4299e12f46fc0f604b7982d0b7f44118fa9dd2
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-08-29 14:24:11 -0500

    Create TestBicycle.java

---
+++ Practica 3/Actividades/TestBicycle.java
@@ -0,0 +1,25 @@
+package testbicycle;
+
+/**
+ *
+ * @author Alumno
+ */
+public class TestBicycle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bicycle a,b;
+        a = new Bicycle("Adam Smith", 1);
+        b = new Bicycle("Ben Jones", 5);
+        a.distanceTraveled(240);
+        System.out.println(b.getOwnerName());
+        b.distaceTraveled(1100);
+        Bicycle mostOld = Bicycle.mostUsed(a,b);
+        System.out.println("mostOld");
+        
+    }
+    
+}
