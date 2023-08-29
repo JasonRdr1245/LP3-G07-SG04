0318ed1a4cd3ae0d6d3e76c8dc8790bbd00917e6
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-08-29 14:43:50 -0500

    Update ContadorTest.java

--- Practica 3/Actividades/ContadorTest.java
+++ Practica 3/Actividades/ContadorTest.java
@@ -17,7 +17,7 @@ public class ContadorTest {
         c1 = new Contador(3);
         c2 = new Contador(10);
         c1.inc();
-        c1.inc();
+        //c1.inc();
         c2.inc();
         System.out.println(c1.getValor());
         System.out.println(c2.getValor());
