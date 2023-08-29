915d11e3e652303592e28cf29b375209cd6ae23a
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-08-29 14:27:25 -0500

    Create Contador.java

---
+++ Practica 3/Actividades/Contador.java
@@ -0,0 +1,28 @@
+package testbicycle;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Contador {
+    static int acumulador = 0;
+    private int valor;
+    
+    public static int acumulador(){
+        return acumulador;
+    }
+    public Contador(int valor){
+        this.valor = valor;
+        acumulador += valor;
+    }
+    public void inc(){
+        valor++;
+        acumulador++;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+    public class ContadorTest{
+        
+    }
+}
