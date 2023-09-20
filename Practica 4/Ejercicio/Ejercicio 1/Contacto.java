528ce313e9cdf3e106cd072dc9ce194b1f991f49
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-09-19 19:40:28 -0500

    Create Contacto.java

---
+++ Practica 4/Ejercicio/Ejercicio 1/Contacto.java
@@ -0,0 +1,37 @@
+package agendacontactos;
+
+/**
+ *
+ * @author user
+ */
+class Contacto {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Contacto(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
