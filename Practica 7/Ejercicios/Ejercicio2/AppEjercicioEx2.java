35fcebdb43c70fb0996e269c572c4c4a609f9938
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-10-30 00:46:42 -0500

    Create AppEjercicioEx2.java

---
+++ Practica 7/Ejercicios/Ejercicio2/AppEjercicioEx2.java
@@ -0,0 +1,34 @@
+package appejericicioex2;
+
+
+public class AppEjericicioEx2 {
+    public static void main(String[] args){
+        try{
+            System.out.println(metodo("w")); 
+        }
+        catch(Exception e){
+            System.out.println("Excepcion en metodo()");
+            e.printStackTrace();
+        }
+    } 
+    private static int metodo(String num){
+        int valor = 0;
+        try{
+            valor ++;
+            valor += Integer.parseInt(num);
+            valor ++;
+            System.out.println("Valor al final del try : "+ valor);
+        }
+        catch(NumberFormatException e){
+            valor += Integer.parseInt("2");
+            System.out.println("Valor al final del catch :"+ valor);
+        } throw e;
+        finally{
+            valor ++;
+            System.out.println("Valor al final de finally : "+valor);
+        }
+        valor ++;
+        System.out.println("Valor antes del return : "+ valor);
+        return valor;
+    }
+ }
