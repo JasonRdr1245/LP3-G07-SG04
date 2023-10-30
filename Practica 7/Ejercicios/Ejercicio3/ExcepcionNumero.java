5f6b0e1cb86b5fa54850b9d9d568ff6759de1891
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-10-30 00:48:38 -0500

    Add files via upload

---
+++ Practica 7/Ejercicios/Ejercicio3/ExcepcionNumero.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appejercicio3ex;
+
+public class ExcepcionNumero extends Exception{ //EXCEPCION creada para Numero
+	public ExcepcionNumero(String mensaje) {
+		super(mensaje);
+	}
+}
---
+++ Practica 7/Ejercicios/Ejercicio3/ExcepcionVocal.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appejercicio3ex;
+
+public class ExcepcionVocal extends Exception{
+	public ExcepcionVocal(String mensaje) { //EXCEPCION creada para vocal
+		super(mensaje);
+	}
+}
