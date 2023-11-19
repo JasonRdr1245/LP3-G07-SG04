3d72dfc60a0ada14d98d8748ece4c46dc392d6ce
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-11-19 15:26:59 -0500

    Create Persona.java

---
+++ Practica10/Actividades/Actividad1/Persona.java
@@ -0,0 +1,15 @@
+package actividad1mg;
+
+/**
+ *
+ * @author user
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+}
