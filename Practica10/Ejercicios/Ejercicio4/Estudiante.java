848cdcb210f9f5fcb4f9567ff9a87ddad087a909
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-11-19 20:49:14 -0500

    Create Estudiante.java

---
+++ Practica10/Ejercicios/Ejercicio4/Estudiante.java
@@ -0,0 +1,42 @@
+// Clase Estudiante que hereda de Persona
+class Estudiante extends Persona {
+    private String carrera;
+
+    public Estudiante(String DNI, String nombres, String direccion, String carrera) {
+        super(DNI, nombres, direccion);
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "carrera='" + carrera + '\'' +
+                "} " + super.toString();
+    }
+    //METODO EQUALS Y HASHCODE HECHO POR IDLE
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estudiante other = (Estudiante) obj;
+        if (!Objects.equals(this.carrera, other.carrera)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
