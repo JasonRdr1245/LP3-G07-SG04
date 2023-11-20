3cc3109502a5fc997fc609472d8a7abda4856d86
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2023-11-19 20:57:49 -0500

    Create Persona.java

---
+++ Practica10/Ejercicios/Ejercicio6/Persona.java
@@ -0,0 +1,66 @@
+import java.util.Objects;
+
+/**
+ *
+ * @author user
+ */
+public class Persona {
+    String nombre;
+    String apellido;
+    public Persona(String nombre, String apellido){
+        this.apellido = apellido;
+        this.nombre = nombre;        
+    }
+    //getters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    //equas y hascode
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        hash = 53 * hash + Objects.hashCode(this.apellido);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellido, other.apellido)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }   
+    
+}
